7aa9b58822555e1cc2d29c55335df8a65269fa9b
Sachin Aryal <saaryal51@gmail.com>
2019-11-11 07:25:02 +0545

    change for heroku.

--- backend/api/token_to_user.py
+++ backend/api/token_to_user.py
@@ -11,15 +11,15 @@ class AuthenticationMiddlewareJWT(object):
     def __call__(self, request):
         try:
             requested_url = request.build_absolute_uri('?')
-            if "api/get_api_token" not in request.build_absolute_uri('?') \
-                    and "/admin" not in requested_url and "/api/" in requested_url:
+            if "api/get_api_token" not in requested_url and "/api/" in requested_url:
+
                 token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[1]
                 data = {'token': token}
                 valid_data = VerifyJSONWebTokenSerializer().validate(data)
                 user = valid_data['user']
                 request.user = user
         except ValidationError as v:
-            return JsonResponse({}, status=401)
+            return JsonResponse({'message': "Not Authorized."}, status=401)
         try:
             data = json.loads(request.body)
             request.params = data
